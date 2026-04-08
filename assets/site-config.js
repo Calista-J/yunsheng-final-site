1feb8e950764a36d69c9f5245cbd8fa58aa900fd
Calista-J <1850988421@qq.com>
2026-04-09 03:34:21 +0800

    Update site-config.js

--- assets/site-config.js
+++ assets/site-config.js
@@ -3,7 +3,7 @@ window.SITE_LINKS = {
   mixVideo: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/mix-video.mp4",
   tweetVideo1: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/tweet-video-1.mp4",
   tweetVideo2: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/tweet-video-2.mp4",
-  backgroundMusic: "#",
+  backgroundMusic: ""https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E7%87%95%E4%BA%91%E5%8D%81%E5%85%AD%E5%A3%B0%2CYida%20-%20%E7%87%95%E4%BA%91%E5%8D%81%E5%85%AD%E5%A3%B0_%E7%99%BB%E5%BD%95%E7%95%8C%E9%9D%A2.mp3"",
   reportDocx: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E4%BA%91%E5%A3%B0%E8%BD%BD%E9%9F%B5%EF%BC%9A%E6%95%B0%E5%AD%97%E8%B5%8B%E8%83%BD%E6%96%87%E5%8C%96%E5%87%BA%E6%B5%B7%E7%BB%93%E9%A1%B9%E6%8A%A5%E5%91%8A.docx",
   reportPdf: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E4%BA%91%E5%A3%B0%E8%BD%BD%E9%9F%B5%EF%BC%9A%E6%95%B0%E5%AD%97%E8%B5%8B%E8%83%BD%E6%96%87%E5%8C%96%E5%87%BA%E6%B5%B7%E7%BB%93%E9%A1%B9%E6%8A%A5%E5%91%8A.pdf",
   newsZip: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E6%96%B0%E9%97%BB%E7%A8%BF.zip",
