c10400e93255f4dae7b50cee8fc8f1a503432464
Calista-J <1850988421@qq.com>
2026-04-09 02:45:54 +0800

    Update site links with new video and document URLs

--- site-config.js
+++ site-config.js
@@ -1,12 +1,12 @@
 window.SITE_LINKS = {
-  finalVideo: "#",
-  mixVideo: "#",
-  tweetVideo1: "#",
-  tweetVideo2: "#",
-  reportDocx: "#",
-  reportPdf: "#",
-  newsZip: "#",
-  tweetZip: "#",
-  finalZip: "#",
-  authorizationImage: "#"
+  finalVideo: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/final-video.mp4",
+  mixVideo: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/mix-video.mp4",
+  tweetVideo1: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/tweet-video-1.mp4",
+  tweetVideo2: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/tweet-video-2.mp4",
+  reportDocx: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E4%BA%91%E5%A3%B0%E8%BD%BD%E9%9F%B5%EF%BC%9A%E6%95%B0%E5%AD%97%E8%B5%8B%E8%83%BD%E6%96%87%E5%8C%96%E5%87%BA%E6%B5%B7%E7%BB%93%E9%A1%B9%E6%8A%A5%E5%91%8A.docx",
+  reportPdf: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E4%BA%91%E5%A3%B0%E8%BD%BD%E9%9F%B5%EF%BC%9A%E6%95%B0%E5%AD%97%E8%B5%8B%E8%83%BD%E6%96%87%E5%8C%96%E5%87%BA%E6%B5%B7%E7%BB%93%E9%A1%B9%E6%8A%A5%E5%91%8A.pdf",
+  newsZip: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E6%96%B0%E9%97%BB%E7%A8%BF.zip",
+  tweetZip: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E6%8E%A8%E6%96%87%E8%A7%86%E9%A2%91.zip",
+  finalZip: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/%E7%BB%93%E9%A1%B9%E8%A7%86%E9%A2%91%E6%9D%90%E6%96%99.zip",
+  authorizationImage: "https://yunshengweb.oss-cn-wuhan-lr.aliyuncs.com/authorization.jpg"
 };
